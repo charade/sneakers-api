d02502f48b712ca5b7a4a0fded1ee458be4081ef
Charles.Ek <macbook@MacBook.local>
2021-11-03 15:19:55 +0100

    :zap:filter by gender/brand/maxPrice

--- src/conrollers/index.js
+++ src/conrollers/index.js
@@ -4,29 +4,40 @@ const  {products, brands} = require('../models');
 
 module.exports = {
     //get all of a brand/gender
-    getAll : async(gender, brand, maxPrice ) => {
+    getAll : async() => {
+        return products.findAll();
+    },
+    //filter by brandsName or gender or maxPrice
+    getGender : async(gender) => {
         let results = await products.findAll({
             where : {
                 gender : {
-                    [Op.like]: `%${ gender ?? '' }%`
-                },
-                brandsName :{
-                    [Op.like]: `%${ brand ?? '' }%`
+                    [Op.startsWith] : `${ gender }`
                 },
             },
-            attributes : {exclude : ["createdAt", "updatedAt"]}
         });
-        //filter by price
-        if(maxPrice){
-            results = results.filter(item => parseInt(item.price) <= maxPrice)
-        }
+        console.log(results)
         return results;
+    },
+    getABrand : async(brand) => {
+        let results = await products.findAll({
+            where : {
+                brandsName : {
+                    [Op.startsWith] : `${ brand }`
+                }
+            },
+        })
+        console.log(results)
+        return results;
+    },
+    getMaxPrice : async(price) => {
+        const results = await products.findAll();
+        return result.filter(item => parseInt(item.price) <= price)
     },
     //get one from id
     getOne : async(id) => {
         const result = await products.findOne({
             where : {id},
-            attributes : {exclude : ["createdAt", "updatedAt"]}
         })
         return result;
     },
--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
-const { getAll, getOne, getBrands } = require('../conrollers');
+const { getAll, getOne, getBrands, getABrand, getGender, getMaxPrice } = require('../conrollers');
 
 //return a genre product
-router.get('/get-all', async(req, res) => {
-   
-    const { gender, brand, maxPrice } = req.query;
-    const results = await getAll(gender, brand, maxPrice);
+router.get('/', async(_, res) => {
+    const results = await getAll();
+    res.status(200).json(results)
+})
+//filter by gender
+router.get('/get-gender/:gender', async(req, res) => {
+    const { gender } = req.params;
+    const results = await getGender(gender);
+    res.status(200).json(results);
+})
+//filter by brand
+router.get('/get-a-brand/:brand', async(req, res) => {
+    const { brand } = req.params;
+    const results = await getABrand(brand);
+    res.status(200).json(results)
+});
+//filter by max price
+router.get('/max-price/:price', async(req, res) => {
+    const { price }  = req.params;
+    const results = await getMaxPrice(price);
     res.status(200).json(results);
 })
 //return details of a sneaker
@@ -14,7 +30,7 @@ router.get('/get-one/:id', async(req, res) => {
     const result = getOne(id);
     res.status(200).json(result);
 })
-
+//return all available brands
 router.get('/get-brands', async(req, res) => {
     const results = await getBrands();
     res.status(200).json(results);
